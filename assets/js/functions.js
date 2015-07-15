c88ddba7a6771cdb41bb6d35762bea4bb4604c43
travisneilson <travisneilson@gmail.com>
2015-07-14 22:06:25 -0700

    spin toggles

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,36 @@
 $( document ).ready(function() {
 
-  // Get started!
+  $('.trigger-list').click(function(){
+    $('.display-type').removeClass('spin');
+    $('.points').css('transform','rotate(' + 0 + 'deg)')
+      .data('rotate', 0);
+  });
+
+  $('.trigger-spin').click(function(){
+    $('.display-type').addClass('spin');
+    $('.points').css('transform','rotate(' + -36 + 'deg)')
+      .data('rotate', -36);
+  });
+
+  $('.trigger-next').click(function(){
+    rotateNext();
+  });
+
+  $('.trigger-prev').click(function(){
+    rotatePrev();
+  });
 
 });
+
+
+function rotateNext(){
+  var rotateData = $('.points').data('rotate');
+  $('.points').css('transform','rotate(' + (rotateData + 36) + 'deg)')
+    .data('rotate', rotateData+36);
+}
+
+function rotatePrev(){
+  var rotateData = $('.points').data('rotate');
+  $('.points').css('transform','rotate(' + (rotateData - 36) + 'deg)')
+    .data('rotate', rotateData-36);
+}
